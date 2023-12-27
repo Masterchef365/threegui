523c29841a24a564ab622c7562e06717ca4f7a17
Duncan Freeman <duncan.freeman1@gmail.com>
2023-12-26 21:10:06 -0800

    Transforms

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! Extension to `epaint` for 3D drawings
+
+use glam::{Mat4, Vec3, Vec4Swizzles};
+use egui::epaint;
+
+pub struct Painter3D {
+    transform: Transform,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// TODO: enum allowing custom transforms
+pub struct Transform(Mat4, Mat4);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Transform {
+    pub fn perspective(fov_y_radians: f32, aspect_ratio: f32, z_near: f32, z_far: f32) -> Self {
+        Self::from_matrix(Mat4::perspective_rh(
+            fov_y_radians,
+            aspect_ratio,
+            z_near,
+            z_far,
+        ))
     }
+
+    pub fn orthographic(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32) -> Self {
+        Self::from_matrix(Mat4::orthographic_rh(left, right, bottom, top, near, far))
+    }
+
+    fn from_matrix(mat: Mat4) -> Self {
+        Self(mat, mat.inverse())
+    }
+
+    pub fn world_to_epaint(&self, world: glam::Vec3) -> epaint::Vec2 {
+        let v: mint::Vector2<f32> = (self.0 * world.extend(1.)).xy().into();
+        v.into()
+    }
+
+    pub fn epaint_to_world(&self, epaint: epaint::Vec2, z: f32) -> glam::Vec3 {
+        let epaint: mint::Vector2<f32> = epaint.into();
+        let epaint: glam::Vec2 = epaint.into();
+        let epaint = epaint.extend(z);
+        (self.1 * epaint.extend(1.)).xyz()
+    }
+}
+
+impl Painter3D {
+
 }
